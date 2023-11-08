da203ee8bba6caead1a316a9db7f103959a18093
stefanfredik <stefanfredik@gmail.com>
2023-11-08 22:06:25 +0800

    fix datasiswa tambah dan upload

--- app/Views/siswa/index.php
+++ app/Views/siswa/index.php
@@ -3,7 +3,7 @@
 <?= $this->section("content"); ?>
 <div class="row">
     <div class="col">
-        <?php if (in_groups("admin")) :  ?>
+        <?php if (in_groups("operator")) :  ?>
             <button data-url="<?= '/' . $meta['url'] . '/tambah'; ?>" class="mb-2 btn btn-cyan" onclick="add(this)"><i class="bi bi-plus-circle mx-1"></i>Tambah Data</button>
             <button data-url="<?= '/' . $meta['url'] . '/upload'; ?>" class="mb-2 btn btn-purple" onclick="add(this)"><i class="bi bi-upload mx-1"></i><span>Upload Excel</span></button>
         <?php endif; ?>
